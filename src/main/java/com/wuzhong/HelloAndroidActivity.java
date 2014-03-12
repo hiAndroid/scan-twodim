28fd2835b0a5b3ae7c611fadacd269ba087c0e7d
wuzhong <wuzhong@taobao.com>
2014-03-12 22:48:42 +0800

    update

--- src/main/java/com/wuzhong/HelloAndroidActivity.java
+++ src/main/java/com/wuzhong/HelloAndroidActivity.java
@@ -1,11 +1,12 @@
 package com.wuzhong;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import com.wuzhong.frag.MyFragmentPageAdapter;
 
-public class HelloAndroidActivity extends Activity {
+public class HelloAndroidActivity extends FragmentActivity {
 
     /**
      * Called when the activity is first created.
@@ -16,15 +17,31 @@ public class HelloAndroidActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+//        ViewPager mViewPager = (ViewPager) findViewById(R.id);
+//
+//        mViewPager.setOffscreenPageLimit(0);
+//
+//        //这里因为是3.0一下版本，所以需继承FragmentActivity，通过getSupportFragmentManager()获取FragmentManager
+//        //3.0及其以上版本，只需继承Activity，通过getFragmentManager获取事物
+//        FragmentManager fm = this.getSupportFragmentManager();
+//        //初始化自定义适配器
+//        MyFragmentPageAdapter mAdapter =  new MyFragmentPageAdapter(fm);
+//        //绑定自定义适配器
+//        mViewPager.setAdapter(mAdapter);
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.wuzhong.R.menu.main, menu);
-	return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//	// Inflate the menu; this adds items to the action bar if it is present.
+//	getMenuInflater().inflate(com.wuzhong.R.menu.main, menu);
+//	return true;
+//    }
 
 }
 
